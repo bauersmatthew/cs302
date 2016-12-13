6b8407b8fac4625bbc1a0e4adf68d27ae9cc46fe
Matthew Bauer <bauer.s.matthew@gmail.com>
2016-12-13 12:45:41 -0800

    Implement RBTree.insert

--- PA07/RBTree.cpp
+++ PA07/RBTree.cpp
@@ -25,3 +25,86 @@ RBTree::~RBTree()
 {
     clear();
 }
+
+void RBTree::insert(int value)
+{
+    if(isEmpty())
+    {
+        // insert as the head node (black)
+        head = new RBTNode(value, nullptr, RBTColor::BLACK);
+        return;
+    }
+    // else
+
+    // insert into tree like a normal binary
+    RBTNode *loc = head;
+    RBTNode **locLoc = &head;
+    RBTNode *last = nullptr;
+    while(loc)
+    {
+        if(value < loc->value)
+        {
+            last = loc;
+            loc = loc->left;
+            locLoc = &loc->left;
+        }
+        else
+        {
+            last = loc;
+            loc = loc->right;
+            locLoc = &loc->right;
+        }
+    }
+    // location found; construct new red node at that spot
+    *locLoc = new RBTNode(value, last);
+
+
+    if(loc->parent->color == RBTColor::BLACK)
+    {
+        // wikipedia case 2; nothing else to do
+        return;
+    }
+    // else
+
+    RBTNode *grandparent = loc->getGrandparent();
+    RBTNode *uncle = loc->getUncle();
+
+    if(uncle && uncle->color == RBTColor::RED)
+    {
+        // wikipedia case 3;
+        loc->parent->color = RBTColor::BLACK;
+        uncle->color = RBTColor::BLACK;
+        grandparent->color = RBTColor::RED;
+        loc->color = RBTColor::BLACK;
+        return;
+    }
+    // else
+
+    if(loc == loc->parent->right && loc->parent == grandparent->left)
+    {
+        // wikipedia case 4 (part 1);
+        rotateLeft(loc->parent);
+        loc = loc->left;
+        // NO RETURN; CONTINUE TO CASE 5
+    }
+    else if(loc == loc->parent->left && loc->parent == grandparent->right)
+    {
+        // wikipedia case 4 (part 2);
+        rotateRight(loc->parent);
+        loc = loc->right;
+        // NO RETURN; CONTINUE TO CASE 5
+    }
+
+    loc->parent->color = RBTColor::BLACK;
+    grandparent->color = RBTColor::RED;
+    if(loc == loc->parent->left)
+    {
+        rotateRight(grandparent);
+    }
+    else
+    {
+        rotateLeft(grandparent);
+    }
+
+    // ok hopefully done now...
+}
